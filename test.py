6534e0f3217dca60cab8ec5a20c37b11a1afc4a7
lolesia <alesyavaskovith@gmail.com>
2025-11-05 10:50:39 +0200

    add: function to upload json playlist

--- test.py
+++ test.py
@@ -1,3 +1,26 @@
+import json
+import random
+
+FILE_NAME = 'songs.json' 
+
+def load_data_from_file(file_path):
+    """
+    Загружает и парсит JSON-данные из указанного файла.
+    """
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            return data
+    except FileNotFoundError:
+        print(f"Ошибка: Файл '{file_path}' не найден.")
+        return None
+    except json.JSONDecodeError:
+        print(f"Ошибка: Некорректный формат JSON в файле '{file_path}'.")
+        return None
+
+playlist_data = load_data_from_file(FILE_NAME)
+
+
 def print_text(text):
     print(text)
 
